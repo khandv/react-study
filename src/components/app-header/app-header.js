1801206bc6e7e615f0827dd2863d28e0de18f7c5
Khan <khandv@gmail.com>
2020-04-15 12:31:19 +0300

    lesson55

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,7 +3,7 @@ import './app-header.css';
 
 const AppHeader = () => {
     return (
-        <div className="app-header" d-flex>
+        <div className="app-header d-flex">
             <h1>Daniil Khan</h1>
             <h2>5 записей, из них понравилось 0</h2>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/';
-// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class WhoAmi extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 26
+        }
+        this.nextYear = this.nextYear.bind(this);
+    }
+    nextYear() {
+        this.setState(state => ({
+            years: ++state.years
+        }))
+    }
+    render() {
+        const {name, surname, link} = this.props;
+        const {years} = this.state;
+        return (
+            <>
+                <button onClick={this.nextYear}></button>
+                <h1>My name is {name}, surname - {surname}, years = {years}</h1>
+                <a href={link}>My profile</a>
+            </>
+        )
+    } 
+}
+
+const All = () => {
+    return (
+        <>
+            <WhoAmi name="John" surname="Smith" link="facebook.com"/>
+            <WhoAmi name="Dan" surname="Khan" link="vk.com"/>
+            <WhoAmi name="Ann" surname="Quinn" link="ok.com"/>
+        </>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
